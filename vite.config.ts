a3ceb06fc53d489a72eccdf2d4db65835ef9e83c
01Barthez <kenwoubarthez@gmail.com>
2024-12-18 15:42:04 +0100

    fix: update vite config setting and add new feature

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { envs } from './src/core/config/env';
+import commonjs from '@rollup/plugin-commonjs';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    commonjs()
+  ],
   server: {
     host: envs.HOST,
     port: envs.PORT,
