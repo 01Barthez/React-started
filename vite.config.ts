80cec786ae622182e27fa6c937e3d17607503c61
01Barthez <kenwoubarthez@gmail.com>
2024-12-18 15:46:41 +0100

    fix: update vite config setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { envs } from './src/core/config/env';
-import commonjs from '@rollup/plugin-commonjs';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(), 
-    commonjs()
-  ],
+  plugins: [ react() ],
 
   server: {
     host: envs.HOST,
