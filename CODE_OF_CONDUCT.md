62700273d72f2aae9743b17c37158557f1b2e641
01Barthez <kenwoubarthez@gmail.com>
2024-12-20 08:19:00 +0100

    fix: Test aceternity and magic ui library

---
+++ CODE_OF_CONDUCT.md
@@ -0,0 +1,21 @@
+# Code of Conduct
+
+## Notre engagement
+
+Nous nous engageons à offrir un environnement respectueux et inclusif pour tous, indépendamment de l’âge, du genre, de l’ethnicité, de l’orientation sexuelle, ou des capacités.
+
+## Nos standards
+
+Voici quelques exemples de comportements qui contribuent à créer un environnement positif :
+
+- Faire preuve d’empathie et de respect envers les autres.
+- Accepter les critiques constructives.
+- Éviter tout langage ou comportement discriminatoire.
+
+## Application
+
+Toute violation sera examinée par les mainteneurs, qui décideront des mesures à prendre. Contactez-nous à [kenwoubarthez@gmail.com] pour signaler un problème.
+
+---
+
+Ce Code de Conduite s'inspire du [Contributor Covenant](https://www.contributor-covenant.org).
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Button } from '@/components/ui/button';
+import Globe from '@/components/ui/globe';
+import { BackgroundBeams } from '@/components/ui/background-beams';
 
 const App: React.FC = () => {
   return (
-    <div className='flex items-center justify-center h-screen flex-col gap-10 w-screen'>
+    <div className='bg-slate-900 flex items-center justify-center min-h-[100vh] flex-col gap-10'>
       <Button
         variant={'outline'}
         className="text-3xl font-bold text-blue-500 p-8"
       >
         Hello world!
       </Button>
-      
+
       <h2 className='text-3xl font-bold'>I'm Barthez</h2>
+
+      <div className="relative flex size-full">
+        <Globe className="top-2" />
+        <div className="pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_200%,rgba(0,0,0,0.2),rgba(255,255,255,0))]" />
+      </div>
+      <BackgroundBeams />
     </div>
   );
 };
