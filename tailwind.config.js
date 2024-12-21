eb2cbe4d08b31b129ce9e5daed80dca7a76675b6
Barthez Kenwou <149054801+01Barthez@users.noreply.github.com>
2024-12-21 17:43:35 +0100

    fix: Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export default {
   content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
   theme: {
     container: {
+      center: true,
       padding: {
         DEFAULT: '1rem',
         sm: '2rem',
