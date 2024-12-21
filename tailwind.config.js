796e3dbbd252e0468f39ea228a106491be1e9d57
Barthez Kenwou <149054801+01Barthez@users.noreply.github.com>
2024-12-21 17:51:40 +0100

    fix: Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -60,10 +60,10 @@ export default {
           4: 'hsl(var(--chart-4))',
           5: 'hsl(var(--chart-5))',
         },
-        fontFamily: {
+      },
+       fontFamily: {
           "poppins": ["Poppins", "Sans-serif"],
         }
-      },
     },
   },
   plugins: [require('tailwindcss-animate')],
